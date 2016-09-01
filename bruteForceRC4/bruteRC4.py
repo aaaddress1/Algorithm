327a781b9876ddb660749b15a73e5227cba9efed
adr macbook <aaaddress1@gmail.com>
2016-09-01 23:54:23 +0800

    no message

--- bruteForceRC4/bruteRC4.py
+++ bruteForceRC4/bruteRC4.py
@@ -4,15 +4,18 @@ date: 2016/9/1
 '''
 from brute import brute
 import re
-
-def rc4crypt( data, key ):
+import time
+ 
+def getSBoxFromKey( key, keylen ):
     s = range(256)
     j = 0
-    keylen = len(key)
     for i in range(256):
         j = ( j + s[i] + ord(key[ i % keylen ]) ) % 256
         s[i] ,s[j] = s[j] ,s[i]
-    
+    return s
+
+def rc4crypt( data, key ):
+    s = getSBoxFromKey( key, len(key) )
     i = j = 0
     out = ''
     for p in range(len(data)):
@@ -22,22 +25,36 @@ def rc4crypt( data, key ):
         out += chr( ord(data[p]) ^ s[ (s[i] + s[j]) % 256 ])
     return out
 
-def checkingASCII( s ):
-    for i in s:
-        if (ord(i) >= 0x7f) or (ord(i) < 0x20):
-            return False
-    return True
+def bruteRC4decrypt( data, key ):
+    s = getSBoxFromKey( key, len(key) )
+    i = j = 0
+    out = ''
+    for p in range(len(data)):
+        i = (i + 1) % 256
+        j = (j + s[i]) % 256
+        s[i],s[j] = s[j],s[i]
+
+        tmp = ord(data[p]) ^ s[ (s[i] + s[j]) % 256 ]
+        if (tmp < 0x7f) and (tmp >= 0x20):
+            out += chr( tmp )
+        else:
+            return ''
+    return out
 
 def bruteRC4( secret, keyMaxLen, regPattern ):
-
+    tStart = time.time()
     for s in brute(length=keyMaxLen):
-        tmp = rc4crypt( secret, s )
-        if checkingASCII(tmp):
-            if re.match( regPattern, tmp):
-                print 'key is "%s"' % s
-                print 'plan text = "%s"' % tmp
+        tmp = bruteRC4decrypt( secret, s )
+        if not tmp:
+            continue
+
+        if re.match( regPattern, tmp):
+            print 'found a result in %f s' % ( time.time() - tStart )
+            print 'key is "%s"' % s
+            print 'plan text = "%s"' % tmp
 
     print 'brute force finish.'
-    return 
+    return;
 
-bruteRC4( rc4crypt('gg in in der','adr'), 5, 'gg.*?' )
+testDeta = rc4crypt('gg in in der','adr')
+bruteRC4( testDeta, 5, 'gg.*?' )
