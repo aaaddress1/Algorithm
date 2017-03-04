1c89a8b3f6a0cccc278500ea86b3ed006672b4ba
adr <aaaddress1@gmail.com>
2017-03-05 02:11:08 +0800

    no message

--- sortingAlgoprithms/sorting.cpp
+++ sortingAlgoprithms/sorting.cpp
@@ -1,25 +1,20 @@
 /**
  * 	@file sorting.cpp
  * 	@author Sheng-Hao Ma (aaaddress1, aka adr)
- * 	@date 02/21/2017
+ * 	@date 03/05/2017
  * 	@brief implementat sorting algoprithms
  **/
 #include <stdio.h>
 #include <algorithm>
 #include <time.h>
-#define swap(type, x, y) do{ type buff = x; x = y; y = buff; } while(0)
-#define calcTime(entermsg, msg, body) { puts(entermsg); clock_t b = clock(); body; printf(msg, ((float)(clock() - b)/CLOCKS_PER_SEC)); }
-
-
-void printArr(/* output array */int *a, /* count */size_t n) {
-	for(size_t i = 0; i < n; printf("%d ", a[i]), i++);
-}
+#define printArr(a, n) { for(size_t i = 0; i < n; printf("%d ", a[i]), i++); }
 
 /* Selection Sorting */
 void selectSort(/* input array */int *a, /* count */size_t n) {
+	#define swap(type, x, y) { type buff = x; x = y; y = buff; }
 	for(int i = 0, j = 0, p = 0; i < n - 1; i++) {
 		p = i;
-		for(j = i + 1; j < n; p = a[p] < a[j] ? p : j, j++);
+		for(j = i + 1; j < n; j++) p = a[p] < a[j] ? p : j;
 		swap(int, a[p], a[i]);
 	}
 }
@@ -29,8 +24,6 @@ void merge(/* input array */int *a, /* left array count */size_t n1, /* right ar
 	int n(n1 + n2);
 	int *tmp(new int[n]);
 	int *left_a(a), *right_a(a + n1);
-	std::sort(left_a, right_a);
-	std::sort(right_a, right_a + n2);
 
 	for (int pos = 0; pos < n; pos++) 
 		if ((left_a >= (a + n1)) && (right_a < a + n1 + n2))
@@ -46,17 +39,25 @@ void merge(/* input array */int *a, /* left array count */size_t n1, /* right ar
 		a[pos] = tmp[pos];
 }
 
-void mergeSort(/* input array */int *a, /* count */size_t n) {
+void mergeSortRecur(/* input array */int *a, /* count */size_t n) {
 	if (n <= 1) return;
 	int half_len = n / 2;
-	mergeSort(a, half_len);
-	mergeSort((a + half_len), (n - half_len));
+	mergeSortRecur(a, half_len);
+	mergeSortRecur((a + half_len), (n - half_len));
 	merge(a, half_len, (n - half_len));
+}
+
+void mergeSortItera(/* input array */int *a, /* count */size_t n) {
+	for (int item_cnt = 1;item_cnt < n; item_cnt *= 2)
+		for (int i = 0; i < n; i += item_cnt *2) {
+			if (i + item_cnt > n) break;
+			merge(a + i, item_cnt, (n-i-item_cnt) > item_cnt ? item_cnt : n-i-item_cnt);
+		}
 }
 
 /* generate data for testing */
 int* genDataArr(int &count) {
-	static int darr[] = {999, 1, 7, 3, 2, 5, 11, 20 , 7, 99, 8, 22, 1000}; //input array data
+	static int darr[] = {999, 1, 7, 3, 2, 9, 22, 8, 0, 5}; //input array data
 	static int n = sizeof(darr) / sizeof(int); //array length
 	int *narr = new int[n];
 	for (int i = 0; i < n; narr[i] = darr[i], i++);
@@ -65,28 +66,18 @@ int* genDataArr(int &count) {
 }
 
 int main(void) {
-	int len, *arr;
-
-	arr = genDataArr(len);
-	calcTime(	"/* === selection sorting case === */",
-				"selection sorting took %f seconds.\n", {
-		printf("curr array at %p\n", arr);
-		puts("curr array: "); printArr(arr, len); puts("");
-		selectSort(arr, len);
-		puts("curr array: "); printArr(arr, len); puts("");
-		delete arr;
-	});
-	puts("");
-
-	arr = genDataArr(len);
-	calcTime(	"/* === merge sorting case === */",
-				"merge sorting took %f seconds.\n", {
-		printf("curr array at %p\n", arr);
-		puts("curr array: "); printArr(arr, len); puts("");
-		mergeSort(arr, len);
-		puts("curr array: "); printArr(arr, len); puts("");
-		delete arr;
-	});
-
+	#define sortClock(data, len, sortName, body) { \
+		int len, *arr = genDataArr(len); \
+		puts(sortName); \
+		printf("\t- data: \n\t\t"); printArr(arr, len); puts(""); \
+		clock_t b = clock(); \
+		body; \
+		printf("\t- %s took %f seconds.\n", sortName, ((float)(clock() - b)/CLOCKS_PER_SEC)); \
+		printf("\t- data after sort: \n\t\t"); printArr(arr, len); puts("\n"); \
+		delete arr; \
+	}
+	sortClock(arr, len, "Selection Sort", { selectSort(arr, len); });
+	sortClock(arr, len, "Recursive Merge Sort", { mergeSortRecur(arr, len); });
+	sortClock(arr, len, "Iterative Merge Sort", { mergeSortItera(arr, len); });
 	return 0;
 }
