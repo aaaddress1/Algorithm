bd5732e2aa8c7681ab4600b96754ff47261aee9b
adr <aaaddress1@gmail.com>
2017-03-08 03:05:09 +0800

    no message

---
+++ sortingAlgoprithms/dev_c_In_windows/main.cpp
@@ -0,0 +1,67 @@
+/**
+* 	@file sorting.cpp
+* 	@author Sheng-Hao Ma (aaaddress1, aka adr)
+* 	@date 03/07/2017
+* 	@brief implementat sorting algoprithms and figure out performance
+**/
+#include <stdio.h>      /* printf, scanf, puts */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time, clock_t, clock */
+#include "heap_sort.cpp"
+#include "merge_sort.cpp"
+
+int* dataGenor(int count) {
+	int c = count, *narr = new int[count];
+	srand(time(0));
+	for (; c-->0; narr[c] = rand() % 100000);
+	return narr;
+}
+
+void insertSort(/* input array */int *a, /* count */int n) {
+   for (int i = 1, tmp = 0, j = 0; i < n; i++) {
+       for (tmp = a[i], j = i-1;j >= 0 && a[j] > tmp;) {
+           a[j+1] = a[j];
+           j = j-1;
+       }
+       a[j+1] = tmp;
+   }
+}
+
+int main(void) {
+    int dataDB[] = { 10000, 50000, 100000, 150000, 200000, 250000 };
+    #define dataDBLen (sizeof dataDB / sizeof *dataDB)
+    #define sortTypeLen (4)
+    char(*sortNm[sortTypeLen]) = { "insertion sort", "recursive merge sort", "iterative merge sort", "heap sort" };
+	void(*funcPtr[sortTypeLen])(int*, int) = { insertSort, mergeSortRecur, mergeSortItera, heapSort };
+
+    /* open file for logs */
+    FILE *fp = fopen("output.txt", "w");
+	if (!fp) {
+		puts("output file open fail.");
+		return 0;
+	}
+	
+    /* print table header */
+	fprintf(fp, "%20s\t", "");
+	for (int i = 0; i < dataDBLen; i++) fprintf(fp, "%8d\t", dataDB[i]);
+	fputs("\n", fp);
+	
+	/* print single list for each sortting algoprithm */
+	for (int mode = 0; mode < sortTypeLen; mode++) {
+		fprintf(fp, "%20s\t", sortNm[mode]);
+		for (int i = 0; i < dataDBLen; i++) {
+            printf("%s is sortting %d numbers.\n", sortNm[mode], dataDB[i]);
+			int *dataArr = dataGenor(dataDB[i]);
+			clock_t b = clock();
+			(*funcPtr[mode])(dataArr, dataDB[i]);
+			fprintf(fp, "%8.3f s\t", (float)(clock() - b) / CLOCKS_PER_SEC);
+			printf("%s took %5.3f s to sort %d numbers.\n", sortNm[mode], (float)(clock() - b) / CLOCKS_PER_SEC, dataDB[i]);
+		}
+		puts("");
+		fputs("\n", fp);
+	}
+	fclose(fp);
+    puts("result table have been saved sucessfully in 'output.txt'!");
+    system("PAUSE");
+	return 0;
+}
--- sortingAlgoprithms/sorting.cpp
+++ sortingAlgoprithms/sorting.cpp
@@ -5,34 +5,66 @@
  * 	@brief implementat sorting algoprithms and figure out performance
  **/
 #include <stdio.h>
+#include <stdlib.h> /* srand, rand */
 #include <time.h>
 #include "heap_sort.cpp"
 #include "merge_sort.cpp"
-#include "data_gentr.cpp"
+
 #define printArr(a, n) { for(size_t i = 0; i < n; printf("%d ", a[i]), i++); }
+#define calcClock(body) ({ clock_t b = clock(); body; (float)(clock() - b)/CLOCKS_PER_SEC; })
+#define dataGenor(count) ({ int c = count, *narr = (int *)malloc(sizeof(int) * count); for(; c-->0; narr[c] = rand() % 100000); narr; })
 
 void insertSort(/* input array */int *a, /* count */size_t n) {
 	for (int i = 1, j, t; i < n; i++) {
-		for(t = a[i], j = i; j --> -1; a[j + 1] = a[j])
-			if (a[j] < t) break;
+		for(t = a[i], j = i; j >= 0 && a[j] > t; a[j + 1] = a[j], j--);
 		a[j + 1] = t;
 	}
 }
 
 int main(void) {
-	#define sortClock(data, len, sortName, body) { \
-		int len, *arr = genDataArr(len); puts(sortName); \
-		printf("\t- data: \n\t\t"); printArr(arr, len); puts(""); \
-		clock_t b = clock(); \
-		body; \
-		printf("\t- %s took %f seconds.\n", sortName, ((float)(clock() - b)/CLOCKS_PER_SEC)); \
-		printf("\t- data after sort: \n\t\t"); printArr(arr, len); puts("\n"); \
-		delete arr; \
-	}
 
-	sortClock(arr, len, "Insertion Sort", { insertSort(arr, len); });
-	sortClock(arr, len, "Recursive Merge Sort", { mergeSortRecur(arr, len); });
-	sortClock(arr, len, "Iterative Merge Sort", { mergeSortItera(arr, len); });
-	sortClock(arr, len, "Heap Sort", { mergeSortItera(arr, len); });
+	int dataDB[] = { 10000, 50000, 100000, 150000, 200000, 250000 };
+	int dataDBLen = sizeof dataDB / sizeof *dataDB;
+
+	printf("%20s\t","");
+	for(int i = 0; i < dataDBLen; i++)
+		printf("%10d %s\t", dataDB[i], "筆");
+	puts("");
+
+	printf("%20s\t", "insertion sort");
+	for (int i = 0; i < dataDBLen; i++) {
+		int *dataArr = dataGenor(dataDB[i]);
+		float t = calcClock({ insertSort(dataArr, dataDB[i]); });
+		printf("%10f %s\t", t, "秒");
+		free(dataArr);
+	}
+	puts("");
+	
+	printf("%20s\t", "recursive merge sort");
+	for (int i = 0; i < dataDBLen; i++) {
+		int *dataArr = dataGenor(dataDB[i]);
+		float t = calcClock({ mergeSortRecur(dataArr, dataDB[i]); });
+		printf("%10f %s\t", t, "秒");
+		free(dataArr);
+	}
+	puts("");
+
+	printf("%20s\t", "iterative merge sort");
+	for (int i = 0; i < dataDBLen; i++) {
+		int *dataArr = dataGenor(dataDB[i]);
+		float t  = calcClock({ mergeSortItera(dataArr, dataDB[i]); });
+		printf("%10f %s\t", t, "秒");
+		free(dataArr);
+	}
+	puts("");
+
+	printf("%20s\t", "heap sort");
+	for (int i = 0; i < dataDBLen; i++) {
+		int *dataArr = dataGenor(dataDB[i]);
+		float t = calcClock({ heapSort(dataArr, dataDB[i]); });
+		printf("%10f %s\t", t, "秒");
+		free(dataArr);
+	}
+	puts("");
 	return 0;
 }
